1436133420b352e6aeccfc57aadacabf962c53d0
penginpenguin <rj_balanay@yahoo.com>
2025-08-06 15:26:03 +0400

    Update text-engine-extension.js

--- text-engine-extension.js
+++ text-engine-extension.js
@@ -1,5 +1,5 @@
 (function (Scratch) {
-    class TextEngineFixed {
+    class TextEngine {
         constructor(runtime) {
             this.runtime = runtime;
             this.fontPrefix = '';
@@ -11,21 +11,30 @@
 
         getInfo() {
             return {
-                id: 'textrendererplus',
-                name: 'Text Engine+',
+                id: 'textengine',
+                name: 'Text Engine',
                 blocks: [
                     {
-                        opcode: 'setFontSettings',
+                        opcode: 'setFont',
                         blockType: Scratch.BlockType.COMMAND,
-                        text: 'set font: sprite [SPRITE] prefix [PREFIX] spacing [SPACING] line spacing [LINESPACING]',
+                        text: 'set font sprite [SPRITE] prefix [PREFIX] spacing [SPACING] line spacing [LINESPACING]',
                         arguments: {
                             SPRITE: {
                                 type: Scratch.ArgumentType.STRING,
                                 menu: 'spriteMenu'
                             },
-                            PREFIX: { type: Scratch.ArgumentType.STRING, defaultValue: '' },
-                            SPACING: { type: Scratch.ArgumentType.NUMBER, defaultValue: 20 },
-                            LINESPACING: { type: Scratch.ArgumentType.NUMBER, defaultValue: 30 }
+                            PREFIX: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: ''
+                            },
+                            SPACING: {
+                                type: Scratch.ArgumentType.NUMBER,
+                                defaultValue: 20
+                            },
+                            LINESPACING: {
+                                type: Scratch.ArgumentType.NUMBER,
+                                defaultValue: 30
+                            }
                         }
                     },
                     {
@@ -33,9 +42,18 @@
                         blockType: Scratch.BlockType.COMMAND,
                         text: 'render text [TEXT] at x [X] y [Y]',
                         arguments: {
-                            TEXT: { type: Scratch.ArgumentType.STRING, defaultValue: 'Hello\nWorld!' },
-                            X: { type: Scratch.ArgumentType.NUMBER, defaultValue: 0 },
-                            Y: { type: Scratch.ArgumentType.NUMBER, defaultValue: 0 }
+                            TEXT: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: 'Hello, world!'
+                            },
+                            X: {
+                                type: Scratch.ArgumentType.NUMBER,
+                                defaultValue: 0
+                            },
+                            Y: {
+                                type: Scratch.ArgumentType.NUMBER,
+                                defaultValue: 0
+                            }
                         }
                     },
                     {
@@ -46,79 +64,3 @@
                 ],
                 menus: {
                     spriteMenu: {
-                        acceptReporters: false
-                    }
-                }
-            };
-        }
-
-        spriteMenu() {
-            return this.runtime.targets
-                .filter(t => t.isOriginal && t.sprite && t.sprite.name)
-                .map(t => t.sprite.name);
-        }
-
-        setFontSettings(args) {
-            this.fontSpriteName = args.SPRITE;
-            this.fontPrefix = args.PREFIX || '';
-            this.spacing = Number(args.SPACING || 20);
-            this.lineSpacing = Number(args.LINESPACING || 30);
-        }
-
-        clearText() {
-            for (const clone of this.lastClones) {
-                if (clone && !clone.isOriginal && this.runtime.targets.includes(clone)) {
-                    clone.dispose();
-                }
-            }
-            this.lastClones = [];
-        }
-
-        renderText(args, util) {
-            const text = args.TEXT || '';
-            const startX = Number(args.X);
-            const startY = Number(args.Y);
-            const lines = text.split('\n');
-
-            const fontSpriteTarget = this.runtime.targets.find(
-                t => t.isOriginal && t.sprite && t.sprite.name === this.fontSpriteName
-            );
-            if (!fontSpriteTarget) return;
-
-            this.clearText();
-
-            const clones = [];
-            let count = 0;
-
-            for (let row = 0; row < lines.length; row++) {
-                const line = lines[row];
-                for (let col = 0; col < line.length; col++) {
-                    const char = line[col];
-                    const costumeName = this.fontPrefix + char;
-
-                    const clone = fontSpriteTarget.makeClone();
-                    if (clone) {
-                        clones.push(clone);
-                        ((clone, y, x, name, delay) => {
-                            setTimeout(() => {
-                                if (clone.setCostume) clone.setCostume(name);
-                                if (clone.setXY) clone.setXY(x, y);
-                            }, delay);
-                        })(clone,
-                           startY - row * this.lineSpacing,
-                           startX + col * this.spacing,
-                           costumeName,
-                           count * 30
-                        );
-                        count++;
-                    }
-                }
-            }
-
-            this.lastClones = clones;
-        }
-    }
-
-    Scratch.extensions.register(new TextEngineFixed());
-})(Scratch);
-
